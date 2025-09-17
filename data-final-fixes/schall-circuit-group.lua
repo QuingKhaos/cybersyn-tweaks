65caa018de89aba379ebd79299ed407ad918df67
Em Karisch <em@khaos.solutions>
2025-09-17 21:09:54 +0200

    feat: add support for SchallCircuitGroup

---
+++ data-final-fixes/schall-circuit-group.lua
@@ -0,0 +1,4 @@
+if mods["SchallCircuitGroup"] then
+  data.raw.item["cybersyn-combinator"].subgroup = "circuit-combinator"
+  data.raw.recipe["cybersyn-combinator"].subgroup = "circuit-combinator"
+end
